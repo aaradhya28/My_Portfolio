c6bb31484b8be2be3d6e0eb5df7b0b2120a7b4a2
aaradhya28 <chandiwadeaaradhya@gmail.com>
2026-04-08 23:04:34 +0530

    Set STATIC_ROOT for PythonAnywhere deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&_qyyk#fc06rxft)jj^n03-fu@xi9=(0_0!q5(@!t$&gwkx(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aaradhyachandiwade.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'   
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
